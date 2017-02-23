f7b1c65399c34eb71b1d3cb81b50609b8b822e6f
Naithar <devias.naith@gmail.com>
2017-02-23 18:43:21 +0300

    stupid implementation for builder and parser.

--- Sources/main.swift
+++ Sources/main.swift
@@ -63,15 +63,17 @@ class Lexer {
             case `func` = "func"
             case `if` = "if"
             case `else` = "else"
+            case `return` = "return"
         }
         
-        case number(Double)
+        case number(Int)
         case `operator`(Operator)
         case bracket(Bracket)
         case keyword(Keyword)
         case separator // ; \n
         case apostrophe // '
         case dot // .
+        case colon // :
         case identifier(String)
         
         init?(character value: UnicodeScalar) {
@@ -79,10 +81,12 @@ class Lexer {
                 self = .bracket(bracket)
             } else if let `operator` = Operator(rawValue: value) {
                 self = .operator(`operator`)
-            } else if [";", "\n"].contains(value) {
+            } else if [";", "\n", "\r"].contains(value) {
                 self = .separator
             } else if value == "." {
                 self = .dot
+            } else if value == ":" {
+                self = .colon
             } else if value == "'" {
                 self = .apostrophe
             } else {
@@ -90,14 +94,16 @@ class Lexer {
             }
         }
         
-        init?(value: String) {
+        init?(string value: String) {
             guard value.unicodeScalars.count > 0 else { return nil }
             
             if let character = Token.init(character: value.unicodeScalars.first!) {
                 self = character
+            } else if ["\r\n", "\n", "\r"].contains(value) {
+                self = .separator
             } else if let keyword = Keyword(rawValue: value) {
                 self = .keyword(keyword)
-            } else if let number = Double(value) {
+            } else if let number = Int(value) {
                 self = .number(number)
             } else {
                 self = .identifier(value)
@@ -146,7 +152,7 @@ class Lexer {
         
         if character.isAlphanumeric {
             let identifier = self.read()
-            return Token(value: identifier)
+            return Token(string: identifier)
         }
         
         return nil
@@ -161,15 +167,362 @@ class Lexer {
     }
 }
 
+enum ParameterType {
+    
+    case unknown
+    case int
+    case double
+    case void
+    
+    init?(value: String) {
+        switch value {
+        case "Int":
+            self = .int
+        case "Double":
+            self = .double
+        case "Void", "":
+            self = .void
+        default:
+            return nil
+        }
+    }
+}
+
+protocol Expressible {
+    
+}
+
+public struct Parameter: Expressible {
+    
+    var name: String = ""
+    var type = ParameterType.unknown
+    
+    init(name: String, type: ParameterType) {
+        self.name = name
+        self.type = type
+    }
+    
+    static func parse(parser: Parser) -> [Parameter]? {
+        guard case .bracket(.parentheses(.open))? = parser.currentToken else {
+            return nil
+        }
+        
+        parser.advance()
+        
+        var result = [Parameter]()
+        loop: while let token = parser.currentToken {
+            switch token {
+            case .bracket(.parentheses(.closed)):
+                break loop
+            default:
+                return nil
+            }
+            
+            parser.advance()
+        }
+        
+        return result
+    }
+}
+
+public struct Return: Expressible {
+    
+    var type = ParameterType.void
+    
+    init?(parser: Parser) {
+        guard case .bracket(.parentheses(.open))? = parser.currentToken else {
+            return nil
+        }
+        
+        parser.advance()
+        
+        guard case .identifier(let name)? = parser.currentToken,
+            let type = ParameterType.init(value: name) else {
+                return nil
+        }
+        
+        self.type = type
+        
+        parser.advance()
+        
+        guard case .bracket(.parentheses(.closed))? = parser.currentToken else {
+            return nil
+        }
+    }
+    
+    init(type: ParameterType = .void) {
+        self.type = type
+    }
+}
+
+struct Body: Expressible {
+    
+    var expressions: [Expression] = []
+    
+    init?(parser: Parser) {
+        
+        var prevToken: Lexer.Token?
+        loop: while let token = parser.currentToken {
+            
+            guard let exp = self.parseToken(token: token, parser: parser, prevToken: &prevToken) else {
+                break loop
+            }
+            
+            if case .none = exp {
+            } else {
+                self.expressions.append(exp)
+            }
+            
+            
+        }
+    }
+    
+    private func parseToken(token: Lexer.Token, parser: Parser, prevToken: inout Lexer.Token?) -> Expression? {
+        switch token {
+        case .bracket(.braces(.open)):
+            parser.advance()
+        case .bracket(.braces(.closed)):
+            return nil
+        case .operator(let `operator`):
+            guard case .number(let left)? = prevToken else {
+                return nil
+            }
+            
+            let lhs = Expression.number(Double(left))
+            parser.advance()
+            
+            guard case .number(let right)? = parser.currentToken else {
+                return nil
+            }
+            
+            let rhs = Expression.number(Double(right))
+            
+            return .binary(lhs, `operator`, rhs)
+        case .keyword(.return):
+            parser.advance()
+            
+            var result: Expression?
+            while result == nil {
+                guard let exp = self.parseToken(token: parser.currentToken!, parser: parser, prevToken: &prevToken) else {
+                    return nil
+                }
+                
+                if case .none = exp {
+                    continue
+                }
+                
+                result = exp
+            }
+            return .return(result!)
+        default:
+            parser.advance()
+        }
+        
+        prevToken = token
+        
+        return Expression.none
+    }
+    
+    init() {}
+    
+    func build(for builder: IRBuilder) {
+        builder.buildRet(builder.buildAdd(IntType.int64.constant(10), IntType.int64.constant(50)))
+    }
+}
+
+indirect enum Expression: Expressible {
+    
+    case none
+    case number(Double)
+    case binary(Expression, Lexer.Token.Operator, Expression)
+    case `return`(Expression)
+    case call(String, [Any])
+    
+    init?(call parser: Parser) {
+        guard case .identifier(let name)? = parser.currentToken else {
+            return nil
+        }
+        
+        parser.advance()
+        parser.advance()
+        
+        self = .call(name, [])
+    }
+    
+    func build(for builder: IRBuilder, to functions: inout [String : LLVM.Function]) {
+        let formatString = builder.buildGlobalStringPtr("Value: %d\n")
+        builder.buildCall(functions["printf"]!, args: [formatString, builder.buildCall(functions["foo"]!, args: [])])
+    }
+}
+
+public struct Function: Expressible {
+    
+    var name: String = ""
+    var parameters: [Parameter] = []
+    var `return`: Return = Return()
+    var body: Body = Body()
+    
+    init?(parser: lang.Parser) {
+        parser.advance()
+
+        guard let nameToken = parser.currentToken,
+            case .identifier(let name) = nameToken else {
+                return nil
+        }
+
+        self.name = name
+        
+        parser.advance()
+        
+        guard let parameters = Parameter.parse(parser: parser) else {
+            return nil
+        }
+        
+        self.parameters = parameters
+        
+        parser.advance()
+        
+        guard let `return` = Return.init(parser: parser) else {
+            return nil
+        }
+        
+        self.return = `return`
+        
+        parser.advance()
+        
+        guard let body = Body(parser: parser) else {
+            return nil
+        }
+        
+        self.body = body
+        
+        parser.advance()
+    }
+    
+    
+    func build(for builder: IRBuilder, to functions: inout [String : LLVM.Function]) {
+        let function = builder.addFunction(self.name, type: self.param())
+        builder.positionAtEnd(of: function.appendBasicBlock(named: "entry"))
+        
+        self.body.build(for: builder)
+        
+        functions[self.name] = function
+    }
+    
+    func param() -> FunctionType {
+        return FunctionType(argTypes: [], returnType: IntType.int64)
+    }
+}
+
+class Program {
+    
+//    indirect enum Expression {
+//        case number(Double)
+//    }
+    
+    public typealias LLVMF = LLVM.Function
+    
+    var expressions: [Expressible] = []
+    
+    init(input: [Expressible]) {
+        self.expressions = input
+    }
+    
+    func run(builder: LLVM.IRBuilder, module: LLVM.Module) {
+        
+        var functions = [String : LLVMF]()
+
+        functions["value"] = builder.addFunction("value", type: FunctionType.init(argTypes: [], returnType: IntType.int64))
+        builder.positionAtEnd(of: functions["value"]!.appendBasicBlock(named: "entry"))
+        builder.buildRet(10)
+        
+        self.expressions.flatMap { $0 as? lang.Function }.forEach { $0.build(for: builder, to: &functions) }
+        
+        
+        let mainType = FunctionType(argTypes: [], returnType: IntType.int64)
+        let function = builder.addFunction("main", type: mainType)
+        let entry = function.appendBasicBlock(named: "entry")
+        builder.positionAtEnd(of: entry)
+        
+        func emitPrintf() -> LLVM.Function {
+            if let function = module.function(named: "printf") { return function }
+            let printfType = FunctionType(argTypes: [PointerType(pointee: IntType.int8)],
+                                          returnType: IntType.int32,
+                                          isVarArg: true)
+            
+            functions["printf"] = builder.addFunction("printf", type: printfType)
+            return functions["printf"]!
+            
+        }
+        
+        let helloString = builder.buildGlobalStringPtr("Hello\n")
+        let formatString = builder.buildGlobalStringPtr("Value: %d\n")
+        let finishString = builder.buildGlobalStringPtr("Finished\n")
+        
+        let printf = emitPrintf()
+        
+        builder.buildCall(printf, args: [helloString])
+        
+        print("start expressions")
+        
+        
+        //    for expression in expressions {
+        //        print("expression - \(expression)")
+        //        switch expression {
+        //        case .binary(let left, let op, let right):
+        //            guard case .number(let left) = left,
+        //                case .number(let right) = right else {
+        //                    continue
+        //            }
+        //
+        //            let leftValue = builder.buildAlloca(type: FloatType.double, name: "left")
+        //            builder.buildStore(FloatType.double.constant(left), to: leftValue)
+        //            let lhs = builder.buildLoad(leftValue)
+        //
+        //            let rightValue = builder.buildAlloca(type: FloatType.double, name: "right")
+        //            builder.buildStore(FloatType.double.constant(right), to: rightValue)
+        //            let rhs = builder.buildLoad(rightValue)
+        //
+        //            print("inside")
+        //            let result: IRValue
+        //
+        //            switch op {
+        //            case .plus:
+        //                result = builder.buildAdd(lhs, rhs)
+        //            default:
+        //                continue
+        //            }
+        //
+        //            builder.buildCall(printf, args: [formatString, result])
+        //        default:
+        //            break
+        //        }
+        //    }
+        
+        builder.buildCall(printf, args: [finishString])
+        
+        let leftValue = builder.buildAlloca(type: IntType.int64, name: "left")
+        let result = builder.buildCall(functions["value"]!, args: [])
+        builder.buildStore(result, to: leftValue)
+        let lhs = builder.buildLoad(leftValue)
+        
+        builder.buildCall(printf, args: [formatString, lhs])
+        
+        self.expressions.flatMap { $0 as? Expression }.forEach { $0.build(for: builder, to: &functions) }
+        
+        builder.buildRet(90)
+    }
+    
+}
 
 class Parser {
     
-    indirect enum Expression {
-        case number(Double)
-        case binary(Expression, Lexer.Token.Operator, Expression)
-        case variable(String)
-        case condition(Expression, Expression, Expression) // if (eX1) { ex2 } ex3 (else lala)
-    }
+//    indirect enum Expression {
+//        case number(Double)
+//        case binary(Expression, Lexer.Token.Operator, Expression)
+//        case variable(String)
+//        case condition(Expression, Expression, Expression) // if (eX1) { ex2 } ex3 (else lala)
+//    }
     
     var tokens: [Lexer.Token]
     
@@ -180,134 +533,72 @@ class Parser {
         self.tokens = input
     }
     
-    private func advance(by count: Int = 1) {
+    func advance(by count: Int = 1) {
         self.index += count
     }
     
-    private func advance(to token: Lexer.Token) throws {
+    func advance(to token: Lexer.Token) throws {
         
     }
     
-    private func advance(with token: Lexer.Token) throws {
+    func advance(with token: Lexer.Token) throws {
         
     }
     
-    private var currentToken: Lexer.Token? {
+    var currentToken: Lexer.Token? {
         return self.index < self.tokens.count ? self.tokens[self.index] : nil
     }
     
-    func parse() -> [Expression] {
-        var result = [Expression]()
+    func parse() -> [Expressible] {
+        var result = [Expressible]()
         
-        while self.currentToken != nil {
-            guard let expression = self.expression() else {
-                continue //TODO: throw error
-            }
-            
-            if case .operator(let `operator`)? = self.currentToken {
-                self.advance()
-                guard let right = self.expression() else {
-                    return [] // TODO: throw error
+        while let token = self.currentToken {
+            switch token {
+            case .keyword(.func):
+                guard let `func` = lang.Function(parser: self) else {
+                    return []
                 }
-                result.append(.binary(expression, `operator`, right))
-            } else {
-                result.append(expression)
+                result.append(`func`)
+            case .identifier(let name):
+                if result.flatMap({ $0 as? lang.Function }).filter({ $0.name == name }).first != nil,
+                    let exp = Expression(call: self) {
+                    result.append(exp)
+                } else {
+                    self.advance()
+                }
+            default:
+                self.advance()
             }
         }
         
         return result
     }
-    
-    private func expression() -> Expression? {
-        guard let token = self.currentToken else {
-            return nil
-        }
-        
-        switch token {
-        case .number(let value):
-            self.advance()
-            return .number(value)
-        case .separator:
-            self.advance()
-        default:
-            break
-        }
-        
-        return nil
-    }
 }
 
+let program = "" +
+    "func foo() (Int) {" +
+        "return 5 + 10" +
+    "}" +
+    "foo()" +
+""
 
-let tokens = Lexer(input: "5 + 1;").tokenize()
+let tokens = Lexer(input: program).tokenize()
 let expressions = Parser(input: tokens).parse()
 
+print("Tokens")
+print(tokens)
+print("Parser")
 print(expressions)
 
 
 func main() throws {
     print("swift print")
     
-    let module: Module = Module(name: "main")
-    let builder: IRBuilder = IRBuilder(module: module)
+    let module: Module = LLVM.Module(name: "main")
+    let builder: IRBuilder = LLVM.IRBuilder(module: module)
+    let program = Program(input: expressions)
     
-        let mainType = FunctionType(argTypes: [], returnType: IntType.int64)
-        let function = builder.addFunction("main", type: mainType)
-        let entry = function.appendBasicBlock(named: "entry")
-        builder.positionAtEnd(of: entry)
-    
-    func emitPrintf() -> Function {
-        if let function = module.function(named: "printf") { return function }
-        let printfType = FunctionType(argTypes: [PointerType(pointee: IntType.int8)],
-                                      returnType: IntType.int32,
-                                      isVarArg: true)
-        return builder.addFunction("printf", type: printfType)
-    }
-    
-    let helloString = builder.buildGlobalStringPtr("Hello\n")
-    let formatString = builder.buildGlobalStringPtr("Value: %f\n")
-    let finishString = builder.buildGlobalStringPtr("Finished\n")
-    
-    let printf = emitPrintf()
-    
-    builder.buildCall(printf, args: [helloString])
-    
-    print("start expressions")
-    for expression in expressions {
-        print("expression - \(expression)")
-        switch expression {
-        case .binary(let left, let op, let right):
-            guard case .number(let left) = left,
-                case .number(let right) = right else {
-                    continue
-            }
-            
-            let leftValue = builder.buildAlloca(type: FloatType.double, name: "left")
-            builder.buildStore(FloatType.double.constant(left), to: leftValue)
-            let lhs = builder.buildLoad(leftValue)
-            
-            let rightValue = builder.buildAlloca(type: FloatType.double, name: "right")
-            builder.buildStore(FloatType.double.constant(right), to: rightValue)
-            let rhs = builder.buildLoad(rightValue)
-            
-            print("inside")
-            let result: IRValue
-            
-            switch op {
-            case .plus:
-                result = builder.buildAdd(lhs, rhs)
-            default:
-                continue
-            }
-            
-            builder.buildCall(printf, args: [formatString, result])
-        default:
-            break
-        }
-    }
-    
-    builder.buildCall(printf, args: [finishString])
-    
-    builder.buildRet(90)
+    program.run(builder: builder, module: module)
     
     try? module.verify()
     
